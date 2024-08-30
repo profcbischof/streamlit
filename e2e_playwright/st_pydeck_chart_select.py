4694f79492b782d9d5da8309eba3825717bec340
Bob Nisco <bob.nisco@snowflake.com>
2024-08-30 12:52:59 -0700

    Better handling of multiple layers

--- e2e_playwright/st_pydeck_chart_select.py
+++ e2e_playwright/st_pydeck_chart_select.py
@@ -29,11 +29,24 @@ def print_on_select():
     st.write("on_select")
 
 
-def get_selected_hex_index_from_session_state() -> int:
+def get_selected_hex_index_from_session_state(layer_id: str) -> int:
     if st.session_state.get("h3_hex_map", {}).get("selection", None) is None:
         return -1
 
+    selection = st.session_state.h3_hex_map.selection
+
+    if selection.layer != layer_id:
+        return -1
+
     return st.session_state.get("h3_hex_map", {}).get("selection", {}).get("index", -1)
+
+
+H3_HEX_DATA_2 = [
+    {"hex": "882830820bfffff", "count": 4},
+    {"hex": "88283095c1fffff", "count": 4},
+    {"hex": "88283095e1fffff", "count": 8},
+]
+df2 = pd.DataFrame(H3_HEX_DATA_2)
 
 
 event_data = st.pydeck_chart(
@@ -41,7 +54,7 @@ event_data = st.pydeck_chart(
         map_style="mapbox://styles/mapbox/outdoors-v12",
         tooltip={"text": "Count: {count}"},
         initial_view_state=pdk.ViewState(
-            latitude=37.7749295, longitude=-122.4194155, zoom=12, bearing=0, pitch=30
+            latitude=37.7749295, longitude=-122.4194155, zoom=11, bearing=0, pitch=30
         ),
         layers=[
             pdk.Layer(
@@ -57,7 +70,29 @@ event_data = st.pydeck_chart(
                 line_width_min_pixels=2,
                 highlight_color=[255, 0, 0],
                 auto_highlight=True,
-                highlighted_object_index=get_selected_hex_index_from_session_state(),
+                highlighted_object_index=get_selected_hex_index_from_session_state(
+                    "MyHexLayer"
+                ),
+            ),
+            pdk.Layer(
+                "H3HexagonLayer",
+                df2,
+                id="MyHexLayer2",
+                pickable=True,
+                stroked=True,
+                filled=True,
+                extruded=True,
+                elevation_scale=20,
+                get_hexagon="hex",
+                get_elevation="count",
+                get_fill_color="[255, (1 - count / 10) * 255, 0]",
+                get_line_color=[255, 255, 255],
+                line_width_min_pixels=2,
+                highlight_color=[255, 0, 0],
+                auto_highlight=True,
+                highlighted_object_index=get_selected_hex_index_from_session_state(
+                    "MyHexLayer2"
+                ),
             ),
         ],
     ),
@@ -70,19 +105,10 @@ event_data = st.pydeck_chart(
 col1, col2 = st.columns(2)
 
 
-def get_selected_hex_index_from_event_data(event_data) -> int:
-    if event_data.get("selection", None) is None:
-        return -1
-
-    return event_data.get("selection", {}).get("index", -1)
-
-
 with col1:
     st.write("### Session State")
-    st.write(get_selected_hex_index_from_session_state())
     st.write(st.session_state)
 
 with col2:
     st.write("### Event Data")
-    st.write(get_selected_hex_index_from_event_data(event_data))
     st.write(event_data)

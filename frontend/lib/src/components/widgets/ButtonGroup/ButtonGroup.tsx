2ed37623cf86317de80aae3a925d271d51e9cec6
Eric Chiu <e.chiu@snowflake.com>
2024-09-06 16:11:34 -0700

    update segments button style

--- frontend/lib/src/components/widgets/ButtonGroup/ButtonGroup.tsx
+++ frontend/lib/src/components/widgets/ButtonGroup/ButtonGroup.tsx
@@ -309,6 +309,13 @@ function ButtonGroup(props: Readonly<Props>): ReactElement {
     mode = MODE.checkbox
   }
 
+  let gap = undefined
+  if (style === ButtonGroupProto.Style.NORMAL) {
+    gap = theme.spacing.none
+  } else if (style === ButtonGroupProto.Style.PILLS) {
+    gap = theme.spacing.twoXS
+  }
+
   const optionElements = options.map((option, index) => {
     const Element = createOptionChild(
       option,
@@ -350,7 +357,7 @@ function ButtonGroup(props: Readonly<Props>): ReactElement {
             style: {
               display: "inline-flex",
               flexWrap: "wrap",
-              gap: theme.spacing.twoXS,
+              gap: `${gap}`,
             },
           },
         }}

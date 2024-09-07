f32bc6a26e4ca1d9b9bbb7a376fa2da7176fc6bd
Eric Chiu <e.chiu@snowflake.com>
2024-09-06 17:12:42 -0700

    button style & make buttons hugging style

--- frontend/lib/src/components/shared/BaseButton/styled-components.ts
+++ frontend/lib/src/components/shared/BaseButton/styled-components.ts
@@ -245,11 +245,15 @@ export const StyledIconButton = styled(
     backgroundColor: theme.colors.transparent,
     border: `${theme.sizes.borderWidth} solid ${theme.colors.borderColor}`,
     borderRadius: theme.radii.none,
-    // padding: iconPadding[size],
-    flex: "1 1 0",
-    padding: 0,
-    maxWidth: minWidth, //theme.sizes.contentMaxWidth,
-    minWidth: minWidth,
+    padding: `${theme.spacing.twoXS} ${theme.spacing.lg}`,
+    // flex: "1 1 0",
+    fontSize: theme.fontSizes.sm,
+    fontWeight: theme.fontWeights.normal,
+    lineHeight: theme.lineHeights.base,
+    maxHeight: theme.sizes.largeLogoHeight,
+    minHeight: theme.sizes.largeLogoHeight,
+    // maxWidth: minWidth, //theme.sizes.contentMaxWidth,
+    // minWidth: minWidth,
     // width: "max-content",
 
     "&:hover": {
@@ -282,7 +286,6 @@ export const StyledIconButtonActive = styled(
     backgroundColor: transparentize(theme.colors.primary, 0.9),
     borderColor: theme.colors.primary,
     color: theme.colors.primary,
-
     "&:hover": {
       backgroundColor: transparentize(theme.colors.primary, 0.8),
       borderColor: theme.colors.primary,
